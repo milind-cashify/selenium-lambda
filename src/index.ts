78b0e64fca181c5f4511c8f7ddf2f24643efb43a
Milind Sharma <milind.s@cashify.in>
2021-07-18 23:11:11 +0530

    lambdatest integration

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,31 @@
 import {Builder, By} from 'selenium-webdriver';
 
+const lambdatestConfig = {
+    username: 'milind.s',
+    accessKey: 'PgKDDj9tNbj6gpmCuiV7cxOiGjsIAPrSgPxYBFWl2dBTzhWjjB',
+    hubLink: 'hub.lambdatest.com/wd/hub'
+}
+
+const getLambdaServerUrl = (): string => {
+    return `https://${lambdatestConfig.username}:${lambdatestConfig.accessKey}@${lambdatestConfig.hubLink}`;
+}
+
+const capabilities = {
+    platform: 'Windows 10',
+    browserName: 'Chrome',
+    version: '92.0',
+    resolution: '1024x768',
+    network: true,
+    visual: true,
+    console: true,
+    video: true,
+    name: 'Test 1', // name of the test
+    build: 'NodeJS build' // name of the build
+}
+
 const startTest = async () => {
-    const driver = new Builder().forBrowser('chrome').build();
+    // const driver = new Builder().forBrowser('chrome').build();
+    const driver = new Builder().withCapabilities(capabilities).usingServer(getLambdaServerUrl()).build();
     await driver.get('https://www.cashify.in');
     const initTitle = await driver.getTitle();
     console.log('Initial Title is:', initTitle);
